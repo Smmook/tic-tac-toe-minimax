490173f94dd89fce487f2ad30fb9007d5932b810
Smmook <diegobix97@gmail.com>
2024-02-04 14:54:58 +0100

    fix win checking

--- game.py
+++ game.py
@@ -14,16 +14,19 @@ def game(player1: str, player2: str) -> None:
         engine = engines[turn % 2]
         move = players.get_move(board, player, engine)
         board = ttt.make_move(board, move, player)
-        if ttt.is_draw(board):
-            break
         if ttt.is_win(board, player):
             winner = player
             break
+        if ttt.is_draw(board):
+            print("Tablas!")
+            ttt.render(board)
+            return
         turn += 1
     ttt.render(board)
     print(f"Player {winner} as {engine} wins!")
     
 if __name__ == "__main__":
-    if sys.argv.__len__() < 3:
-        raise Exception("Uso: python game.py engine1 engine2")
-    game(sys.argv[1], sys.argv[2])
+    # if sys.argv.__len__() < 3:
+    #     raise Exception("Uso: python game.py engine1 engine2")
+    # game(sys.argv[1], sys.argv[2])
+    game("human", "minimax")
--- players.py
+++ players.py
@@ -30,6 +30,14 @@ def get_move(board: list[list[str]], player: str, engine: str) -> tuple[int,int]
 def minimax_ai(board: list[list[str]], player: str) -> tuple[int,int]:
     max_eval = -9999
     best_move = None
+    
+    if board == [
+        ["X", "O", None],
+        ["O", "O", "X"],
+        ["X", "X", None]
+    ]:
+        breaking = "bad"
+    
     for move in ttt.get_legal_moves(board):
         state = ttt.make_move(board, move, player)
         eval = minimax(state, ttt.get_opponent(player), False, alpha=-9999, beta=9999)
@@ -44,10 +52,12 @@ def minimax(board: list[list[str]], player: str, maximizing: bool, alpha: int, b
     if state_str in cache:
         return cache[state_str]
     
-    if ttt.is_draw(board):
-        return 0
     if ttt.is_win(board, player):
         return 10 if maximizing else -10
+    if ttt.is_win(board, ttt.get_opponent(player)):
+        return -10 if maximizing else 10
+    if ttt.is_draw(board):
+        return 0
     
     max_eval = -9999
     min_eval = 9999
